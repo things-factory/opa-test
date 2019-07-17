1fd33ef220bb84fdb73245d4547131ccf2ce2504
leemyunggyu <jaylee@hatiolab.com>
2019-07-17 17:34:48 +0900

    add dependency and migration files

---
+++ server/migrations/1563352450357-SeedVendor.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class SeedVendor1563352450357 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
