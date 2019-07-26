afa9228cb26a31f3d6472a74cfa75a5851d87fa8
horwengliang <horwengliang@gmail.com>
2019-07-26 18:29:26 +0800

    migration seed product

--- server/migrations/1563446367842-SeedProduct.ts
+++ server/migrations/1563446367842-SeedProduct.ts
@@ -1,11 +1,33 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { Product } from '@things-factory/product-base'
+import path from 'path'
+import { getRepository, MigrationInterface, QueryRunner } from 'typeorm'
+import { csvHeaderCamelizer } from '@things-factory/shell'
+
+const seedFilePath = '../../seeds/product.csv'
 
 export class SeedProduct1563446367842 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    const products = await csvHeaderCamelizer(path.resolve(__dirname, seedFilePath))
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
+    try {
+      await getRepository(Product).save(products)
+    } catch (e) {
+      console.error(e)
     }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<any> {
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    const products = await csvHeaderCamelizer(path.resolve(__dirname, seedFilePath))
+
+    try {
+      await getRepository(Product)
+        .createQueryBuilder()
+        .delete()
+        .from(Product)
+        .where('name IN (:...names)', { names: products.map(product => product.name) })
+        .execute()
+    } catch (e) {
+      console.error(e)
     }
-
+  }
 }
