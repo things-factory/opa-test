ae071e2b3dc0bb21b0a2eea84502e8650d7b73e5
horwengliang <horwengliang@gmail.com>
2019-07-27 23:21:46 +0800

    v0.0.32
    fix menu column and add seeds, migration transport vehicle

--- server/migrations/1563449592181-SeedTransportVehicle.ts
+++ server/migrations/1563449592181-SeedTransportVehicle.ts
@@ -1,11 +1,39 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { TransportVehicle } from '@things-factory/transport-base'
+import path from 'path'
+import { getRepository, MigrationInterface, QueryRunner } from 'typeorm'
+import { Domain, csvHeaderCamelizer } from '@things-factory/shell'
+
+const seedFilePath = '../../seeds/transport-vehicle.csv'
 
 export class SeedTransportVehicle1563449592181 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    const transportVehicles = await csvHeaderCamelizer(path.resolve(__dirname, seedFilePath))
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
+    for (let i = 0; i < transportVehicles.length; i++) {
+      const transportVehicle = transportVehicles[i]
+      transportVehicle.domain = await getRepository(Domain).findOne({ where: { name: 'SYSTEM' } })
+      console.log(transportVehicle)
     }
 
-    public async down(queryRunner: QueryRunner): Promise<any> {
+    try {
+      await getRepository(TransportVehicle).save(transportVehicles)
+    } catch (e) {
+      console.error(e)
     }
+  }
 
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    const transportVehicles = await csvHeaderCamelizer(path.resolve(__dirname, seedFilePath))
+
+    try {
+      await getRepository(TransportVehicle)
+        .createQueryBuilder()
+        .delete()
+        .from(TransportVehicle)
+        .where('name IN (:...names)', { names: transportVehicles.map(transportVehicle => transportVehicle.name) })
+        .execute()
+    } catch (e) {
+      console.error(e)
+    }
+  }
 }
