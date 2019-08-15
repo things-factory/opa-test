34c1cf40cb3f369364a1114e47df99533d3be028
leemyunggyu <jaylee@hatiolab.com>
2019-08-15 12:45:18 +0800

    v0.0.54

--- server/migrations/1563419512900-SeedBizplace.ts
+++ server/migrations/1563419512900-SeedBizplace.ts
@@ -1,23 +1,31 @@
 import { Bizplace, Company } from '@things-factory/biz-base'
+import { csvHeaderCamelizer } from '@things-factory/shell'
 import path from 'path'
 import { getRepository, MigrationInterface, QueryRunner } from 'typeorm'
-import { csvHeaderCamelizer } from '@things-factory/shell'
 
 const seedFilePath = '../../seeds/bizplace.csv'
 
 export class SeedBizplace1563352365741 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     const bizplaces = await csvHeaderCamelizer(path.resolve(__dirname, seedFilePath))
+    const companies = await getRepository(Company).find()
 
-    for (let i = 0; i < bizplaces.length; i++) {
-      const bizplace = bizplaces[i]
-      bizplace.parent = await getRepository(Company).findOne({ where: { name: bizplace.parentName } })
-    }
+    for (let i = 0; i < companies.length; i++) {
+      const company = companies[i]
+      const _bizplaces = []
 
-    try {
-      await getRepository(Bizplace).save(bizplaces)
-    } catch (e) {
-      console.error(e)
+      for (let j = 0; j < bizplaces.length; j++) {
+        const bizplace = { ...bizplaces[j] }
+        bizplace.company = company
+        bizplace.name = `${bizplace.name}-${i}`
+        _bizplaces.push(bizplace)
+      }
+
+      try {
+        await getRepository(Bizplace).save(_bizplaces)
+      } catch (e) {
+        console.error(e)
+      }
     }
   }
 
