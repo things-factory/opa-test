e91067225b023b74eae12c0614f9e6568cbeafc6
Hearty Oh <heartyoh@hatiolab.com>
2019-09-24 11:48:13 +0800

    v0.0.103

--- server/migrations/1569212995746-SeedBoardModel.ts
+++ server/migrations/1569212995746-SeedBoardModel.ts
@@ -20,7 +20,7 @@ const BOARDS = {
     {
       name: 'LOCATION',
       description: 'Location Label Model',
-      model: `{"width": 400, "height": 300}`,
+      model: `{"width":800,"height":320,"type":"model-layer","translate":{"x":0,"y":194.57500000000002},"scale":{"x":1.13875,"y":1.13875},"fillStyle":"#fff","left":0,"top":0,"components":[{"type":"barcode","symbol":"qrcode","text":"","left":136.82617391680537,"top":40.40134661113845,"width":239.9999999999999,"height":239.99999999999997,"rotation":0,"id":"location","mappings":[{"target":"(self)","rule":"value","property":"text"},{"target":"#location-text","rule":"value","property":"text"}],"data":"X"},{"type":"text","left":430.96558082120083,"top":50.73156868796167,"width":273.3853924552041,"height":190.54175732740606,"text":"","fillStyle":"#fff","strokeStyle":"#000","alpha":1,"hidden":false,"lineWidth":5,"lineCap":"butt","textAlign":"center","textBaseline":"middle","textWrap":false,"fontFamily":"serif","fontSize":"240","rotation":0,"id":"shelf","mappings":[{"target":"(self)","rule":"value","property":"text"}],"data":"X"},{"type":"text","left":400.1244575004591,"top":231.74955369524707,"width":335.0676390966875,"height":67.61565836298934,"text":"","fillStyle":"#fff","strokeStyle":"#000","alpha":1,"hidden":false,"lineWidth":5,"lineCap":"butt","textAlign":"center","textBaseline":"middle","textWrap":false,"fontFamily":"serif","fontSize":"60","rotation":0,"mappings":[{"target":"(self)","rule":"value","property":"text"}],"data":"X","id":"location-text"}]}`,
       thumbnail: ''
     },
     {
