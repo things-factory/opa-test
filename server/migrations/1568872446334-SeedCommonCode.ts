20e6bba1da94b314bcc792ef9197d0953414caa4
leemyunggyu <jaylee@hatiolab.com>
2019-09-28 22:03:53 +0800

    v0.2.16

--- server/migrations/1568872446334-SeedCommonCode.ts
+++ server/migrations/1568872446334-SeedCommonCode.ts
@@ -6,155 +6,86 @@ const commonCodes = [
   {
     name: 'LOCATION_TYPE',
     description: 'Location type',
-    commonCodeDetails: [
-      {
-        name: 'BUFFER',
-        description: 'buffer_location'
-      },
-      {
-        name: 'SHELF',
-        description: 'shelf'
-      }
-    ]
+    commonCodeDetails: [{ name: 'BUFFER', description: 'buffer_location' }, { name: 'SHELF', description: 'shelf' }]
   },
   {
     name: 'ORDER_STATUS',
     description: 'Order status',
     commonCodeDetails: [
-      {
-        name: 'PENDING',
-        description: 'order_status_pending'
-      },
-      {
-        name: 'EDITING',
-        description: 'order_status_editing'
-      },
-      {
-        name: 'PENDING_RECEIVE',
-        description: 'order_status_pending_receive'
-      },
-      {
-        name: 'INTRANSIT',
-        description: 'order_status_intransit'
-      },
-      {
-        name: 'ARRIVED',
-        description: 'order_status_arrived'
-      },
-      {
-        name: 'PROCESSING',
-        description: 'order_status_processing'
-      },
-      {
-        name: 'DONE',
-        description: 'order_status_done'
-      }
+      { name: 'PENDING', description: 'order_status_pending' },
+      { name: 'EDITING', description: 'order_status_editing' },
+      { name: 'PENDING_RECEIVE', description: 'order_status_pending_receive' },
+      { name: 'INTRANSIT', description: 'order_status_intransit' },
+      { name: 'ARRIVED', description: 'order_status_arrived' },
+      { name: 'PROCESSING', description: 'order_status_processing' },
+      { name: 'DONE', description: 'order_status_done' }
     ]
   },
   {
     name: 'ORDER_PRODUCT_STATUS',
     description: 'Order Product status',
     commonCodeDetails: [
-      {
-        name: 'PENDING',
-        description: 'product_status_pending'
-      },
-      {
-        name: 'INTRANSIT',
-        description: 'product_status_intransit'
-      },
-      {
-        name: 'ARRIVED',
-        description: 'product_status_arrived'
-      },
-      {
-        name: 'UNLOADING',
-        description: 'product_status_unloading'
-      },
-      {
-        name: 'UNLOADED',
-        description: 'product_status_unloaded'
-      },
-      {
-        name: 'PUTTING AWAY',
-        description: 'product_status_putting_away'
-      },
-      {
-        name: 'STORED',
-        description: 'product_status_stored'
-      }
+      { name: 'PENDING', description: 'product_status_pending' },
+      { name: 'INTRANSIT', description: 'product_status_intransit' },
+      { name: 'ARRIVED', description: 'product_status_arrived' },
+      { name: 'UNLOADING', description: 'product_status_unloading' },
+      { name: 'UNLOADED', description: 'product_status_unloaded' },
+      { name: 'PUTTING AWAY', description: 'product_status_putting_away' },
+      { name: 'STORED', description: 'product_status_stored' }
     ]
   },
   {
     name: 'ORDER_VAS_STATUS',
     description: 'Order Vas status',
     commonCodeDetails: [
-      {
-        name: 'PENDING',
-        description: 'vas_status_pending'
-      },
-      {
-        name: 'PROCESSING',
-        description: 'vas_status_processing'
-      }
+      { name: 'PENDING', description: 'vas_status_pending' },
+      { name: 'PROCESSING', description: 'vas_status_processing' }
     ]
   },
   {
     name: 'LOAD_TYPES',
     description: 'Load types',
     commonCodeDetails: [
-      {
-        name: 'FCL',
-        description: 'full_container_load'
-      },
-      {
-        name: 'LCL',
-        description: 'low_container_load'
-      }
+      { name: 'FCL', description: 'full_container_load' },
+      { name: 'LCL', description: 'low_container_load' }
     ]
   },
   {
     name: 'WORKSHEET_TYPES',
     description: 'worksheet types',
     commonCodeDetails: [
-      {
-        name: 'UNLOADING',
-        description: 'unloading'
-      },
-      {
-        name: 'PUTAWAY',
-        description: 'putaway'
-      },
-      {
-        name: 'VAS',
-        description: 'VAS'
-      },
-      {
-        name: 'LOADING',
-        description: 'laoding'
-      },
-      {
-        name: 'PICKING',
-        description: 'picking'
-      }
+      { name: 'UNLOADING', description: 'unloading' },
+      { name: 'PUTAWAY', description: 'putaway' },
+      { name: 'VAS', description: 'VAS' },
+      { name: 'LOADING', description: 'laoding' },
+      { name: 'PICKING', description: 'picking' }
     ]
   },
   {
     name: 'WORKSHEET_STATUS',
     description: 'worksheet status',
     commonCodeDetails: [
-      {
-        name: 'DEACTIVATED',
-        description: 'deactivated'
-      },
-      {
-        name: 'EXECUTING',
-        description: 'executing'
-      },
-      {
-        name: 'DONE',
-        description: 'done'
-      }
+      { name: 'DEACTIVATED', description: 'deactivated' },
+      { name: 'EXECUTING', description: 'executing' },
+      { name: 'DONE', description: 'done' }
+    ]
+  },
+  {
+    name: 'WEIGHT_UNITS',
+    description: 'Unit for weight',
+    commonCodeDetails: [{ name: 'kg', description: 'kilogram' }, { name: 't', description: 'ton' }]
+  },
+  {
+    name: 'PACKING_TYPES',
+    description: 'Types of packing',
+    commonCodeDetails: [
+      { name: 'CORRUGATED_BOX', description: 'Corrugated box packing' },
+      { name: 'CARTON', description: 'Carton packing' },
+      { name: 'DRUM', description: 'Drum packing' },
+      { name: 'BOTTLE', description: 'Bottle packing' },
+      { name: 'SACK', description: 'Sack packing' },
+      { name: 'BOXBOARD', description: 'Boxboard packing' },
+      { name: 'CAN', description: 'Can packing' }
     ]
   }
 ]
